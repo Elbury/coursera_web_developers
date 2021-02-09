9ca9cd4fbd0a9ccbc6f6aa04114ce3ba46f795ba
El Bury <buryfredy@gmail.com>
2021-02-09 11:29:01 -0500

    plugin HTML, css-loader & style-loader, cleanWebpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,25 @@
 const path = require('path');
-
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+   new CleanWebpackPlugin(),
+   new HtmlWebpackPlugin({
+    title: 'Development',
+  }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
   },
 };
